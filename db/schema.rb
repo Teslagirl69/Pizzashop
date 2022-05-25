ab58bb97c175baf0bc8b18085ffa57640411293d
Teslagirl69 <teslagirl17@gmail.com>
2022-05-26 02:02:34 +0300

    change images + migrate "add product"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_221247) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_223100) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
