6eb42f73962177a9557afa969b341606322c3b4c
Teslagirl69 <teslagirl17@gmail.com>
2022-05-26 20:45:47 +0300

    fix mifrations + add Display st2

--- db/migrate/20220526173753_add_products.rb
+++ db/migrate/20220526173753_add_products.rb
@@ -3,8 +3,7 @@ class AddProducts < ActiveRecord::Migration[7.0]
 
   Product.create :title => 'Hawaiian', :description => 'This is a Hawaiian pizza with chicken, pineapple and cheese.', :price => 450, :size => 30, :is_spicy => false, :is_veg => false, :best_offer => false, :path_to_image => '/Images/hawaii.jpg'
   Product.create :title => 'Pepperoni',:description => 'This is a pepperoni pizza with salami and cheese in tomato paste.', :price => 350, :size => 30, :is_spicy => true, :is_veg => false, :best_offer => true, :path_to_image => '/Images/pepperoni.jpg'
-  Product.create :title => 'Hawaiian', :description => 'This is a vegan pizza with only vegetables.', :price => 550, :size => 30, :is_spicy => false, :is_veg => true, :best_offer => false, :path_to_image => '/Images/veg.jpg'
-
+  Product.create :title => 'Vegeterian', :description => 'This is a vegan pizza with only vegetables.', :price => 550, :size => 30, :is_spicy => false, :is_veg => true, :best_offer => false, :path_to_image => '/Images/veg.jpg'
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_223100) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_173753) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
